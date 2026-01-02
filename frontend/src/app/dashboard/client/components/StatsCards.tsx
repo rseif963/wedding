f28ddfcc4b1c52bc7f0c3588a39f7ea744fbc4e9
rseif963 <rseif0380@gmail.com>
2026-01-02 12:44:52 +0300

    Update StatsCards.tsx
    updated

--- frontend/src/app/dashboard/client/components/StatsCards.tsx
+++ frontend/src/app/dashboard/client/components/StatsCards.tsx
@@ -66,7 +66,7 @@ export default function StatsCards() {
   ];
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6 px-6 w-full mt-2">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6 px-2 w-full mt-2">
       {stats.map((s, i) => (
         <div
           key={s.label}
